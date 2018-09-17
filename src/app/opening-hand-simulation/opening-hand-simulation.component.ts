0538d0ad800e8ddb95e90b10bb63e92dcc90b2df
Christopher Chen <christopher.chen1995@gmail.com>
2018-09-17 13:41:32 -0700

    Basic simulation implemented

--- src/app/opening-hand-simulation/opening-hand-simulation.component.ts
+++ src/app/opening-hand-simulation/opening-hand-simulation.component.ts
@@ -1,5 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { CardParserService } from "../card-parser.service";
+function shuffle(array) {
+  //Fisher-Yates shuffle
+  var i = 0,
+    j = 0,
+    temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+class HandRecord {
+  hand: string[];
+  message: string;
+  constructor(_hand: string[], _message: string) {
+    this.hand = _hand;
+    this.message = _message;
+  }
+}
+
+
+
 @Component({
   selector: "app-opening-hand-simulation",
   templateUrl: "./opening-hand-simulation.component.html",
@@ -7,6 +32,40 @@ import { CardParserService } from "../card-parser.service";
 })
 export class OpeningHandSimulationComponent implements OnInit {
   constructor(private cardParser: CardParserService) {}
+  cards: string[] = [];
+  hand: string[] = [];
+  history: HandRecord[] = [];
+  scoreboard: { [message: string]: number } = {};
+  scoreboardArray = [];
 
-  ngOnInit() {}
+  handCategories = [
+    {name:"Good"},
+    {name:"Bad"},
+  ]
+
+  ngOnInit() {
+    this.cards = this.cardParser.cards.slice(0, this.cardParser.cards.length);
+    this.newHand();
+  }
+
+  newHand() {
+    shuffle(this.cards);
+    this.hand = this.cards.slice(0, 7);
+  }
+
+  recordHand(hand: string[], message: string) {
+    this.history.push(new HandRecord(hand, message));
+    if (this.scoreboard[message] == undefined) {
+      this.scoreboard[message] = 0;
+    }
+    console.log(this.scoreboard);
+    this.scoreboard[message] += 1;
+    let scores = Object.keys(this.scoreboard).map(k => {
+      return [ this.scoreboard[k],k];
+    });
+    scores.sort((a,b) =>{return a[0] - b[0]});
+    scores.reverse();
+    this.scoreboardArray = scores; 
+    this.newHand();
+  }
 }
